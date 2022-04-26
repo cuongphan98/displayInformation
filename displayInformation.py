cf9efe2f05bc9ad516152611a45c3fa571a04acf
Cuong Phan <cuongphan98ete@gmail.com>
2022-04-26 14:14:17 +0700

    bai tap

---
+++ displayInformation.py
@@ -0,0 +1,8 @@
+Name = "Phan Quang Cuong"
+Date_of_birth = "12-01-1998"
+Adress = "Quang Ngai"
+Purpose = 'hoc python de phuc vu cong viec'
+print(Name)
+print(Date_of_birth)
+print(Adress)
+print(Purpose )
