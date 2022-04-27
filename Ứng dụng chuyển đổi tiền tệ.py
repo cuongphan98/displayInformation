7cf4ae1155ba8f631eb929604a6629f2a4a617ac
Cuong Phan <cuongphan98ete@gmail.com>
2022-04-27 21:14:58 +0700

    bt chuyen doi tien te

---
+++ Ứng dụng chuyển đổi tiền tệ.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import math
+
+USD =int(input("Nhập số tiền USD cần đổi: "))
+VND =int(input("Nhập tỉ giá USD/VND: "))
+result = USD*VND
+
+print("kết quả là: "+ str(result))
+
+
+# In[ ]:
+
+
+
+
