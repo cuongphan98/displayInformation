5f5739c4892f3aac41a2c66d2bd25344ab0d0ddc
cuongphan98 <103835931+cuongphan98@users.noreply.github.com>
2022-05-07 17:01:23 +0700

    Add files via upload

---
+++ [Bài tập] Hiển thị ngày trong tuần.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+day = int(input("nhap so: "))
+if day == 1:
+    print("Monday")
+elif day == 2:
+    print("Tuesday")
+elif day == 3:
+    print("Wednesday")
+elif day == 4:
+    print("Tuesday")
+elif day == 5:
+    print("Friday")
+elif day == 6:
+    print("Saturday")
+elif day == 7:
+    print("Sunday")
+else:
+    print("error, out of range")
+
+
+# In[ ]:
+
+
+
+
