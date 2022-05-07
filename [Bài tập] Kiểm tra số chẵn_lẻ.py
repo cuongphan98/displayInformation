29746578e130b668af965e309984fb5a8a5c533d
cuongphan98 <103835931+cuongphan98@users.noreply.github.com>
2022-05-07 16:52:26 +0700

    Add files via upload

---
+++ [Bài tập] Kiểm tra số chẵn_lẻ.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+a = input()
+a = float(a)
+if a % 2 == 0:
+    print('số chẵn')
+elif a % 2 == 1:
+    print('số lẻ')
+else:
+    print('không phải số tự nhiên')
+
+
+# In[ ]:
+
+
+
+
