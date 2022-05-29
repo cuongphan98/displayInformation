b86ea15c41b9a8a3b301962538800167b77010a4
cuongphan98 <103835931+cuongphan98@users.noreply.github.com>
2022-05-29 15:36:34 +0700

    Add files via upload

---
+++ bangcuuchuong.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#bang cuu chuong
+for a in range (2,10):
+    print("bang cuu chuong",str(a))
+    for b in range (1,11):
+        print(a,'x',b,'=', a*b)
+    print()
+
+
+# In[ ]:
+
+
+
+
