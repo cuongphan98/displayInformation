c1681366ae134fdd48a91ff4bbb3c4636c8a45fb
cuongphan98 <103835931+cuongphan98@users.noreply.github.com>
2022-07-04 21:26:41 +0700

    Add files via upload

---
+++ Count number of occurrence of words in a given text.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def word_count(str):
+    counts = dict()
+    words = str.split()
+
+    for word in words:
+        if word in counts:
+            counts[word] += 1
+        else:
+            counts[word] = 1
+
+    return counts
+
+print( word_count('the quick brown fox jumps over the lazy dog.'))
+
